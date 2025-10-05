81527a9a6c19f03aa26aedf8e60aee843c2745f2
VulcanWM <logmedha09@gmail.com>
2025-10-05 11:47:15 +0100

    started coordinate geometry file

---
+++ math-forge/coordinate-geometry.py
@@ -0,0 +1,4 @@
+def distance_between_two_points(a: float, b: float, x: float, y: float) -> float:
+    """Returns the distance between the point (a, b) and (x, y)."""
+
+    return abs(((x-a)**2 + (y-b)**2)**0.5)
