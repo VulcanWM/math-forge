096b580d1b2e5151088813b87deabe473bee25f0
VulcanWM <logmedha09@gmail.com>
2025-10-05 09:02:52 +0100

    added is_prime and prime_factors

--- math-forge/number-theory.py
+++ math-forge/number-theory.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def gcd(a: int, b: int) -> int:
     """Greatest common divisor of two integers using Euclid's algorithm."""
 
@@ -9,3 +12,24 @@ def lcm(a: int, b: int) -> int:
     """Least common multiple of two integers"""
 
     return abs(a * b) // gcd(a, b)
+
+def is_prime(n: int) -> bool:
+    """Returns True if n is prime, False otherwise."""
+
+    num_of_factors = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            num_of_factors += 1
+
+    return num_of_factors == 2
+
+def prime_factors(n: int) -> List[int]:
+    """Returns a list of prime factors of n."""
+
+    n_prime_factors = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            if is_prime(i):
+                n_prime_factors.append(i)
+
+    return n_prime_factors
